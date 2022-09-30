a847bd84ae63e76a3902d1b5a674e7674cb9dcfc
Mihail Vacarciuc <mishgun032@yandex.com>
2022-09-30 22:01:46 +0100

    updated the todo list component

---
+++ src/components/styledComponents/todo.js
@@ -0,0 +1,158 @@
+import styled from 'styled-components'
+
+export const StyledInput = styled.input`
+  font-size: 25px;
+  width: 100%;
+  box-sizing: border-box;
+
+  &:focus {
+  outline: none;
+  }
+`
+
+export const StyledTodoItem = styled.div`
+  display:flex;
+  flex-wrap: wrapp;
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  hyphens: auto;
+  white-space: normal;
+  width: 100%;
+
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  
+  box-sizing: border-box;
+  background: white;
+
+  text-align: center;
+  justify-content: center;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  font-size: 20px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+`
+
+export const TodoContainer = styled.div`
+  overflow: scroll;
+  max-height: 700px;
+  box-sizing: border-box;
+  padding-right: 50px;
+
+`
+
+export const TodoItemContainer = styled.div`
+  position: relative;
+  z-index: 1;
+  padding-right: 30px;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  flex-direction: column;
+  margin-bottom: 15px;
+  align-items: center;
+
+`
+
+export const DeleteBtn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: transparent;
+  font-weight: bold;
+  width : 20px;
+  height: 20px;
+  border-radius: 100%;
+  color: #933939;
+  background: white;
+  fontWeight: bold;
+  fontSize: 20px;
+  maxHeight:40p;
+  
+  border: none;
+`
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #71797E;
+  width: 100%;
+  align-items: center;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  color: white;
+  font-size: 17px;
+`
+
+export const DescriptionButton = styled.button`
+  width: 100%;
+  background: #404447;
+  border: none;
+  color: white;
+  font-weight: bold;
+  font-size: 17px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  cursor: pointer;
+`
+
+export const DescriptionContentContainer = styled.form`
+  width: 100%;
+`
+
+export const DescriptionContent = styled.textarea`
+  margin: 0;
+  background: #71797E;
+  font-size: 15px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  height: 50px;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 2px solid black;
+  width: 100%;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  color: white;
+  &:focus {
+  outline: none;
+  }
+`
+
+export const DescritionInputContainer = styled.form`
+  width: 100%;
+`
+
+export const DescriptionInput = styled.input`
+  width: 100%;
+  margin: 0px;
+  text-align: center;
+  background: #5b6266;
+  border: none;
+  box-sizing: border-box;
+  color: white;
+  font-size: 20px;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+
+  &:focus {
+  outline: none;
+  }
+
+`
+
+export const InputContainer = styled.form`
+  display: flex;
+  width: 100%;
+  margin-bottom: 30px;
+
+`
+
--- src/components/todo.js
+++ src/components/todo.js
@@ -1,156 +1,154 @@
 import React, { useState, useMemo, useEffect } from 'react';
 import styled from 'styled-components'
 import { v4 as uuidv4 } from 'uuid';
-
-class TodoWrapper extends React.Component {
+import {StyledInput,TodoContainer,TodoItemContainer,StyledTodoItem,DeleteBtn,DescriptionContainer,DescriptionButton,DescriptionContentContainer,DescriptionContent,DescriptionInput,DescritionInputContainer,InputContainer} from './styledComponents/todo'
+class TodoWrapper extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       todoItems: [],
-      inputValues: ""
+      inputValue: ""
     }
     this.handlDeleteItem = this.handlDeleteItem.bind(this)
     this.handleSubmitItem = this.handleSubmitItem.bind(this)
     this.handleInput = this.handleInput.bind(this)
+    this.handleAddDescription = this.handleAddDescription.bind(this)
+    this.handleChangeDescription = this.handleChangeDescription.bind(this)
   }
-  componentWillMount(){
+  componentDidMount(){
     const storedTodoItems = localStorage.getItem("todoItems") ? JSON.parse(localStorage.getItem("todoItems")) : []
-    console.log(storedTodoItems)
     this.setState({todoItems: storedTodoItems})
-    console.log(storedTodoItems)
   }
   componentDidUpdate(prevProps,prevState){
     if( !(prevState.todoItems != this.state.todoItems)) return;
-    console.log(JSON.stringify(this.state.todoItems))
     localStorage.setItem("todoItems",JSON.stringify(this.state.todoItems))
   }
 
   handleSubmitItem(e){
     e.preventDefault()
     this.setState( prevState => ({
-      todoItems: [...prevState.todoItems,prevState.inputValue]
+      todoItems: [{title: prevState.inputValue,description: ""},...prevState.todoItems],
+      inputValue: ""
     }))
   }
   handleInput(e){
-    this.setState( prevState => ({
-      input: prevState.input+e.target.value
-    }))
+    this.setState({ inputValue: e.target.value })
   }
   handlDeleteItem(index){
     const todoItemsArr = [...this.state.todoItems]
     todoItemsArr.splice(index,1)
-    this.setState( prevState => ({
-      todoItems: [...prevState.todoItems.slice(index,1)]
-    }))
+    this.setState({todoItems:  todoItemsArr})
   }
-  
+  handleAddDescription(e,index,desc){
+    e.preventDefault()
+    let todoItems = [...this.state.todoItems]
+    if(todoItems[index].description.length === 0){
+      todoItems[index].description = [desc]
+    }else todoItems[index].description = [desc,todoItems[index].description]
+    this.setState({todoItems: todoItems})
+  }
+  handleChangeDescription(e,index,desc,descIndex){
+    e.preventDefault()
+    let todoItems = [...this.state.todoItems]
+    todoItems[index].description[descIndex] = desc
+    this.setState({todoItems: todoItems})
+  }
   render() {
-    return <Todo todoItems={this.state.todoItems}
-    handleSubmitItem={this.handleSubmitItem}
-    handleInput={this.handleInput}
-    handlDeleteItem={this.handlDeleteItem}/>
+    return <Todo
+	     todoItems={this.state.todoItems}
+	     inputValue={this.state.inputValue}
+	     handleSubmitItem={this.handleSubmitItem}
+	     handleInput={this.handleInput}
+	     handlDeleteItem={this.handlDeleteItem}
+    handleAddDescription={this.handleAddDescription}
+    handleChangeDescription={this.handleChangeDescription} />
   }
 };
 
-function Todo({todoItems,inputValue,handleSubmitItem,handleInput,handlDeleteItem}){
-  const TodoItemsContainerWrapp = useMemo( () => TodoItemsContainer({todoItems,handlDeleteItem}),[todoItems])
+function Todo({todoItems,inputValue,handleSubmitItem,handleInput,handlDeleteItem,handleAddDescription,handleChangeDescription}){
+  const TodoItemsContainerWrapp = useMemo( () => TodoItemsContainer({todoItems:todoItems,handlDeleteItem,handleAddDescription,handleChangeDescription}),[todoItems])
   return (
     <StyledTodo>
       <InputContainer onSubmit={handleSubmitItem}>
-	<input style={styledInput} name="" type="text" onChange={handleInput} value={inputValue} />
-	<button style={submitBtn} onClick={handleSubmitItem}>Submit</button>
+	<StyledInput name="" type="text" onChange={handleInput} value={inputValue} />
       </InputContainer>
       {TodoItemsContainerWrapp}
     </StyledTodo>
   );
 }
       
-function TodoItemsContainer({todoItems,handlDeleteItem}) {
-  console.log("render")
+function TodoItemsContainer({todoItems,handlDeleteItem,handleAddDescription,handleChangeDescription}) {
   return (
     <TodoContainer>
       {
 	todoItems.map( (todoItem,index) => {
-	  return < TodoItem text={todoItem} deleteItem={() => handlDeleteItem(index)} key={uuidv4()}/>
+	  return (< TodoItem text={todoItem}
+	    deleteItem={() => handlDeleteItem(index)}
+	    key={uuidv4()}
+	    handleAddDesc={handleAddDescription} 
+	    index={index}
+	    handleChangeDescription={handleChangeDescription}/>
+	  )
 	})
       }
     </TodoContainer>
-    
   )
 }
 
-function TodoItem({text,deleteItem}) {
+function TodoItem({text,deleteItem,handleAddDesc,index,handleChangeDescription}) {
   return (
     <TodoItemContainer>
-      <StyledTodoItem>{text}</StyledTodoItem>
-      <button style={submitBtn} onClick={deleteItem}>delete</button>
+      <StyledTodoItem>{text.title}</StyledTodoItem>
+      <DeleteBtn onClick={deleteItem}>X</DeleteBtn>
+      <TodoItemDescription desc={text.description}
+			   handleAddDesc={handleAddDesc}
+			   index={index}
+			   handleChangeDescription={handleChangeDescription} />
     </TodoItemContainer>
   )
 }
 
-const InputContainer = styled.form`
-  display: flex;
-  width: 100%;
-  margin-bottom: 30px;
-`
-
-const styledInput = {
-  "fontSize": "20px",
-  "width": "272px",
-  "boxSizing": "border-box",
+function TodoItemDescription({desc,handleAddDesc,index,handleChangeDescription}){
+  const [showDescription,setShowDescription] = useState(false)
+  const [inputValue,setInputValue] = useState("")
+  const [textAreatValue,setTextAreaValue] = useState(desc)
+  function handleShowDescription(index){
+    setShowDescription(!showDescription)
+  }
+  function handleDescInput(e,index){
+    let textArea = [...textAreatValue] 
+    textArea[index] = e.target.value
+    setTextAreaValue(textArea)
+  }
+  return (
+    <DescriptionContainer>
+    { (showDescription || (desc.length === 1 && desc[0].length < 30) ) &&
+      desc.map( (description,descIndex) => {
+	return (
+	  <DescriptionContentContainer onSubmit={(e) => handleChangeDescription(e,index,description,descIndex)}>
+	    <DescriptionContent value={textAreatValue[descIndex]} onChange={ e => handleDescInput(e,descIndex)} />
+	  </DescriptionContentContainer>
+      )})
+    }
+    { ((desc && desc.length !== 0 && showDescription) || !(desc && desc.length !== 0) || (desc.length === 1 && desc[0].length < 30)) &&
+      <DescritionInputContainer onSubmit={(e) => handleAddDesc(e,index,inputValue)}>
+	<DescriptionInput placeholder="Description" value={inputValue}  onChange={(e) => setInputValue(e.target.value)} />
+      </DescritionInputContainer>
+    }
+    {  (desc && desc.length !== 0)  ? (desc.length !== 1) && 
+      <DescriptionButton onClick={handleShowDescription}>
+	{showDescription ? "Hide Description" : "Show Description"}
+      </DescriptionButton> : null
+      
+    }
+    </DescriptionContainer>
+  )
 }
+
 
 const StyledTodo = styled.div`
   width: 400px;
- height: 500px;
+  height: 500px;
 `
-
-const TodoContainer = styled.div`
-  
-`
-
-const StyledTodoItem = styled.div`
-  display:flex;
-  flex-wrap: wrapp;
-  min-height: 50px;
-  overflow-wrap: break-word;
-word-wrap: break-word;
-  hyphens: auto;
-  white-space: normal;
-  width: 277px;
-
-  padding-left: 20px;
-  padding-right: 20px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-
-  box-sizing: border-box;
-  background: white;
-`
-
-const TodoItemContainer = styled.div`
-  display: flex;
-  justifyContent: space-between;
-  margin-bottom: 15px;
-  align-items: center;
-`
-
-
-const submitBtn = {
-  "background": "#933939",
-  "width" : "110px",
-  "color": "white",
-  "fontWeight": "bold",
-  "fontSize": "20px",
-  "maxHeight":"40px",
-  "paddingTop": "5px",
-  "paddingBottom": "5px",
-  "paddingLeft": "20px",
-  "paddingRight": "20px",
-
-  "border": "none",
-
-  "marginLeft": "20px"
-}
 
 export default TodoWrapper;
