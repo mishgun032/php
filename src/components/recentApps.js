9fa0bb7dbd6e65c2d79b178c9619a9dc73b1aa58
Mihail Vacarciuc <mishgun032@yandex.com>
2022-10-04 10:20:52 +0100

    made the recent apps components fully functional

--- src/components/recentApps.js
+++ src/components/recentApps.js
@@ -1,41 +1,64 @@
-import React, {useState} from 'react';
-import * as styled from './styledComponents/recentApps'
+import React, {useState,useMemo} from 'react';
+import * as Styled from './styledComponents/recentApps'
 
 
 function RecentApps(){
-  const addMore = (name,url) => {
+  const addMore = (event,name,url) => {
     alert('f')
+    event.preventDefault()
+    setApps([{name:name,url:url},...apps])
   }
 
   const [apps, setApps] = useState(localStorage.getItem("apps") ? JSON.parse(localStorage.getItem("apps")) : [] )
   const [showPopup,setShowPopup] = useState(false)
   return (
-    <styled.RecentAppsWrapp>
-      {showPopup && <Popup handleSubmit={addMore} />}
-      {
-	apps.map( (app,index) => {
-	  return <App details={app} key={index} />
-	  
-	})
-      }
-      <styled.App onClick={ () =>  setShowPopup(!showPopup)}><h1>+</h1></styled.App>
-    </styled.RecentAppsWrapp>
+    <Styled.RecntAppsContainer>
+      <Styled.RecentAppsWrapp>
+	<AppContainer apps={apps}></AppContainer>
+	<Styled.App onClick={ () =>  setShowPopup(!showPopup)}>+</Styled.App>
+	{showPopup && <Popup submit={addMore} />}
+      </Styled.RecentAppsWrapp>
+    </Styled.RecntAppsContainer>
+  )
+}
+
+function AppContainer({apps}) {
+  console.log(apps)
+  return (
+    <>
+    {
+      apps.map( (app,index) => {
+	return (
+	  <App name={app.name} url={app.url} />
+	)
+      })
+    }
+    </>
   )
 }
 
 function App({name,url}){
   console.log(name)
   return (
-    <styled.App>
-      <h1>kjk</h1>
-      {name}
-    </styled.App>
+    <Styled.App>
+      <a href={url} method="get" target="_blank">{name}</a>
+    </Styled.App>
   )
 }
 
-function Popup(){
+function Popup({submit}){
+  const [name,setName] = useState("")
+  const [url,setUrl] = useState("")
   return (
-    <div></div>
+    <Styled.PopupWrapp>
+      <Styled.AddApp onSubmit={(e) => submit(e,name,url)}>
+	<Styled.AddAppTitle>Name</Styled.AddAppTitle>
+	<Styled.Input value={name} onChange={(e) => setName(e.target.value)}/>
+	<Styled.AddAppTitle>Url</Styled.AddAppTitle>
+	<Styled.Input value={url} onChange={(e) => setUrl(e.target.value)}/>
+	<button onSubmit={submit}>click me</button>
+      </Styled.AddApp>
+    </Styled.PopupWrapp>
   )
 }
 
--- src/components/styledComponents/recentApps.js
+++ src/components/styledComponents/recentApps.js
@@ -1,14 +1,52 @@
 import styled from 'styled-components'
 
+export const RecntAppsContainer = styled.div`
+  min-width: 50vw;
+  max-width: 50vw;
+  overflow: scroll;
+`
+
 export const RecentAppsWrapp = styled.main`
   display: flex;
-  justify-content: space-between;
-  max-width: 80wh;
-  width: 80%;
-  height: 150px;
-
+  align-items: center;
+  height: 100px;
+overflow: scroll;
 `
 export const App = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
   width: 50px;
   height: 50px;
+  margin-left: 10px;
+  margin-right: 10px;
+  cursor: pointer;
+  font-size: 20px;
+`
+
+export const PopupWrapp = styled.main`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  padding-top: 100px;
+  box-sizing: border-box;
+  top: 100px;
+  left: 0;
+  width: 100vw;
+  height: 70vh;
+  background: black;
+`
+
+export const AddApp = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+
+export const AddAppTitle = styled.label`
+
+`
+
+export const Input = styled.input`
+
 `
