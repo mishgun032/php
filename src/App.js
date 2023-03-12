21ec76efce11bb931dc95f485f7d480274881c86
Mihail <mishgun032@yandex.com>
2023-03-12 20:30:32 +0000

    added an option to triger events by typing specific words

--- src/App.js
+++ src/App.js
@@ -12,43 +12,68 @@ import Popup from './components/popup'
 
 import styled from 'styled-components'
 
-import { useState,useRef,useEffect} from 'react'
+import { useState, useRef, useLayoutEffect } from 'react'
 
 export const keyCodes = {"0":48,"1":49,"2":50,"3":51,"4":52,"5":53,"6":54,"7":55,"8":56,"9":57,"d":68,"b":66,"a":65,"s":83,"i":73,"f":70,"k":75,"+":187,"p":80,"o":79,"u":85,"z":90,"t":84,"r":82,"e":69,"w":87,"g":71,"h":72,"j":74,"l":76,"#":191,"y":89,"x":88,"c":67,"v":86,"n":78,"m":77,",":188,".":190,"-":189,"ArrowRight":39,"ArrowLeft":37,"ArrowUp":38,"ArrowDown":40,"PageDown":34,"Clear":12,"Home":36,"PageUp":33,"End":35,"Delete":46,"Insert":45,"Control":17,"AltGraph":18,"Meta":92,"Alt":18,"Shift":16,"CapsLock":20,"Tab":9,"Escape":27,"F1":112,"F2":113,";":188,":":190,"_":189,"'":191,"*":187,"Q":81,"W":87,"E":69,"R":82,"T":84,"Z":90,"S":83,"A":65,"D":68,"I":73,"U":85,"O":79,"Y":89,"X":88,"C":67,"F":70,"V":86,"G":71,"B":66,"H":72,"N":78,"J":74,"M":77,"K":75,"L":76,"P":80,"Ö":192,"Ä":222,"Ü":186,"!":49,"\"":50,"§":51,"$":52,"%":53,"&":54,"/":55,"(":56,")":57,"=":48,"*":56,"?":219,"°":220}
 
+
 function App() {
+  console.log('updated app.js')
   const [currentBg,setCurrentBg] = useState(localStorage.getItem('bg') ? localStorage.getItem('bg') : 'default.png')
   const [err,setErr] = useState(false)
   const [errMsg,setErrMsg] = useState("")
   const searchBarRef = useRef()
-  const hotkeys = {}
+  const hotkeys = useRef()
+  const keywords = useRef()
+    
+  function setWord(word,fn){
+    for(let i=0;i<keywords.current.length;i++){if(keywords.current[i].wordObject.word == word) return;};
+    keywords.current.push({letter: word[0], wordObject: {word: Array.from(word),current_index: 0},onType: () => fn()})
+  }
   function setHotkey(key,fn){
-    if(key in hotkeys){
+    if(Object.keys(hotkeys.current).indexOf(key) !== -1){
       setErr(true)
       setErrMsg(`${key} is already in use`)
-      return;
+      return false;
     }
     if(!(key in keyCodes)){
       setErr(true)
       setErrMsg("invalid key")
-      return;
+      return false;
     }
-    hotkeys[key] = fn;
+    hotkeys.current[key] = fn;
+    return true;
   }
   const deleteHotkey = key => {
     alert('here')
     alert(key)
-    delete hotkeys[key]
+    delete hotkeys.current[key]
   }
-  useEffect( () => {
+  useLayoutEffect( () => {
+    hotkeys.current = {}
+    keywords.current= []
     document.body.addEventListener('keydown', e => {
       if(e.ctrlKey && e.shiftKey){
-	if (Object.keys(hotkeys).indexOf(e.key) !== -1){
-	  hotkeys[e.key]();
+	if (Object.keys(hotkeys.current).indexOf(e.key) !== -1){
+	  hotkeys.current[e.key]();
+          return;
 	}
       }else if(e.ctrlKey && e.key === "Enter"){
 	searchBarRef.current.focus()
+        return;
       }
+      if(document.activeElement.tagName === "INPUT" || document.activeElement.tagName === "TEXTAREA") return
+      keywords.current.forEach( (obj) => {
+        if(obj.letter === e.key){
+          if(obj.wordObject.current_index < obj.wordObject.word.length-1){
+            obj.wordObject.current_index += 1
+            obj.letter=obj.wordObject.word[obj.wordObject.current_index]
+            return;
+          }
+          obj.onType()
+        }
+        obj.wordObject.current_index = 0;obj.letter=obj.wordObject.word[0]
+      })
     })
   },[])
 
