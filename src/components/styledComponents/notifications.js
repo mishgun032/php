9fdea249b7f157969937aaa3cdbcfe0458c1ab38
Mihail <mishgun032@yandex.com>
2023-05-27 18:16:30 +0100

    styles for the notifications

---
+++ src/components/styledComponents/notifications.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const Contianer = styled.div`
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  border-radius: 5px;
+  z-index: 999999;
+  background-color: ${props => props.color};
+  color: white;
+  padding: 30px;
+  overflow: auto;
+  width: 300px;
+  max-width: 300px;
+  word-wrap: break-word   ;
+  height: 80px;
+  text-align: center;
+
+  & h3 {
+    font-weight: 300;
+  }
+  & button {
+    position: absolute;
+    top: 12px;
+    right: 5px;
+    background: none;
+    color: white;
+    border: none;
+    font-size: medium;
+    cursor: pointer;
+  }
+`
