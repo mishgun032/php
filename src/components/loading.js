36004b51e1c090d723ba25fa5a7c663b9af217fc
Mihail <mishgun032@yandex.com>
2023-05-28 02:15:03 +0100

    added a loading spinner component which doesnt work yet ))

---
+++ src/components/loading.js
@@ -0,0 +1,7 @@
+import "./styles/loading.module.css"
+
+export default function LoadingSpinner(){
+  return (
+    <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+  )
+}
